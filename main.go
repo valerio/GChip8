dc71be5f5787ce70f592a28e365999ab35d52c63
Valerio Pipolo <valep27@gmail.com>
2017-02-08 23:23:06 +0100

    Change main to use frontend interface

--- main.go
+++ main.go
@@ -1,33 +1,20 @@
 package main
 
-import "fmt"
-import "github.com/veandco/go-sdl2/sdl"
+import (
+	"fmt"
+
+	"github.com/valep27/chip8/io"
+	"github.com/valep27/chip8/emu"
+)
 
 func main() {
 	fmt.Println("Starting emulator...")
 
-	sdl.Init(sdl.INIT_EVERYTHING)
+	chip8 := emu.New()
+	front := io.NewSdlFrontend()
 
-	window, err := sdl.CreateWindow("Chip8",
-		sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		800, 600,
-		sdl.WINDOW_SHOWN | sdl.WINDOW_OPENGL)
+	front.Initialize()
+	defer front.Close()
 
-	if err != nil {
-		panic(err)
-	}
-
-	defer window.Destroy()
-
-	surface, err := window.GetSurface()
-	if err != nil {
-		panic(err)
-	}
-
-	rect := sdl.Rect{0, 0, 800, 600}
-	surface.FillRect(&rect, 0xffff0000)
-	window.UpdateSurface()
-
-	sdl.Delay(3000)
-	sdl.Quit()
+	front.Draw(chip8)
 }
