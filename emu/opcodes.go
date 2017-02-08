1c557b9bb957a7141badda9aabb0f747869a16b6
Valerio Pipolo <valep27@gmail.com>
2017-02-09 00:28:41 +0100

    Add implementation of opcode 6XNN

--- emu/opcodes.go
+++ emu/opcodes.go
@@ -8,5 +8,15 @@ var OpcodeMap = map[uint16]OpcodeFunc {
 // Nop does nothing
 // This is not an actual opcode, just a placeholder.
 func Nop(c8 *Chip8) {
-    
+    c8.pc += 2
+}
+
+// SetVxToImmediate implements opcode 6XNN.
+// It will set NN (8 bit immediate) to the register Vx.
+func SetVxToImmediate(c8 *Chip8) {
+    x := (c8.opcode & 0x0F00) >> 8
+    nn := uint8(c8.opcode & 0x00FF)
+
+    c8.V[x] = nn
+    c8.pc += 2
 }
