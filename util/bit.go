5aeb6dca818124560fae51a8a38f6a86ad016918
Valerio Pipolo <valep27@gmail.com>
2017-02-11 12:18:13 +0100

    Fix comment of checked add

--- util/bit.go
+++ util/bit.go
@@ -6,7 +6,7 @@ func CombineBytes(low, high uint8) uint16 {
     return (uint16(high) << 8) | uint16(low)
 }
 
-// CheckedAdd adds two 8 bit unsigned values and detects if an overflow
+// CheckedAdd adds two 8 bit unsigned values and detects if an overflow happened.
 func CheckedAdd(a, b uint8) (result uint8, overflow bool) {
     overflow = false
     highBits := (uint16(a) + uint16(b)) & 0xFF00
