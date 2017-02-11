65a8945947d1c9bb7290591bbf4b6f8577afafbb
Valerio Pipolo <valep27@gmail.com>
2017-02-11 12:17:25 +0100

    Add checked add function and tests

--- util/bit.go
+++ util/bit.go
@@ -5,3 +5,16 @@ package util
 func CombineBytes(low, high uint8) uint16 {
     return (uint16(high) << 8) | uint16(low)
 }
+
+// CheckedAdd adds two 8 bit unsigned values and detects if an overflow
+func CheckedAdd(a, b uint8) (result uint8, overflow bool) {
+    overflow = false
+    highBits := (uint16(a) + uint16(b)) & 0xFF00
+
+    if highBits > 0 {
+        overflow = true
+    }
+
+    result = a + b
+    return
+}
--- util/bit_test.go
+++ util/bit_test.go
@@ -1,6 +1,8 @@
 package util
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCombineBytes(t *testing.T) {
 	type args struct {
@@ -12,14 +14,42 @@ func TestCombineBytes(t *testing.T) {
 		args args
 		want uint16
 	}{
-		{ "combines max values", args{0xFF, 0xFF}, 0xFFFF },
-		{ "combines values", args{0xAB, 0xBA}, 0xBAAB },
-		{ "combines zero", args{0x00, 0x00}, 0x0000 },
+		{"combines max values", args{0xFF, 0xFF}, 0xFFFF},
+		{"combines values", args{0xAB, 0xBA}, 0xBAAB},
+		{"combines zero", args{0x00, 0x00}, 0x0000},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := CombineBytes(tt.args.low, tt.args.high); got != tt.want {
 				t.Errorf("CombineBytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCheckedAdd(t *testing.T) {
+	type args struct {
+		a uint8
+		b uint8
+	}
+	tests := []struct {
+		name         string
+		args         args
+		wantResult   uint8
+		wantOverflow bool
+	}{
+		{"add 10 and 250 overflows", args{10, 250}, 4, true},
+		{"add 124 and 90 overflows", args{124, 90}, 214, false},
+		{"add 255 and 1 overflows", args{255, 1}, 0, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotResult, gotOverflow := CheckedAdd(tt.args.a, tt.args.b)
+			if gotResult != tt.wantResult {
+				t.Errorf("CheckedAdd() gotResult = %v, want %v", gotResult, tt.wantResult)
+			}
+			if gotOverflow != tt.wantOverflow {
+				t.Errorf("CheckedAdd() gotOverflow = %v, want %v", gotOverflow, tt.wantOverflow)
 			}
 		})
 	}
