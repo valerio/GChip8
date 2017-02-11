67d03cc961a080c91a54c8a0fe3a5e97454a2b20
Valerio Pipolo <valep27@gmail.com>
2017-02-11 13:27:39 +0100

    Add checked subtraction and tests

--- util/bit_test.go
+++ util/bit_test.go
@@ -39,7 +39,7 @@ func TestCheckedAdd(t *testing.T) {
 		wantOverflow bool
 	}{
 		{"add 10 and 250 overflows", args{10, 250}, 4, true},
-		{"add 124 and 90 overflows", args{124, 90}, 214, false},
+		{"add 124 and 90 does not overflow", args{124, 90}, 214, false},
 		{"add 255 and 1 overflows", args{255, 1}, 0, true},
 	}
 	for _, tt := range tests {
@@ -50,6 +50,34 @@ func TestCheckedAdd(t *testing.T) {
 			}
 			if gotOverflow != tt.wantOverflow {
 				t.Errorf("CheckedAdd() gotOverflow = %v, want %v", gotOverflow, tt.wantOverflow)
+			}
+		})
+	}
+}
+
+func TestCheckedSub(t *testing.T) {
+	type args struct {
+		a uint8
+		b uint8
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantResult uint8
+		wantBorrow bool
+	}{
+		{"subtract 10 and 250 borrows", args{10, 250}, 16, true},
+		{"subtract 124 and 90 does not borrow", args{124, 90}, 34, false},
+		{"subtract 0 and 1 borrows", args{0, 1}, 255, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotResult, gotBorrow := CheckedSub(tt.args.a, tt.args.b)
+			if gotResult != tt.wantResult {
+				t.Errorf("CheckedSub() gotResult = %v, want %v", gotResult, tt.wantResult)
+			}
+			if gotBorrow != tt.wantBorrow {
+				t.Errorf("CheckedSub() gotBorrow = %v, want %v", gotBorrow, tt.wantBorrow)
 			}
 		})
 	}
