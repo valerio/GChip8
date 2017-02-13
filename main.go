e0838534cf782f38038e2ec9d048182b4b4d0b4a
Valerio Pipolo <valep27@gmail.com>
2017-02-13 23:32:53 +0100

    Add simple game loop logic

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/valep27/GChip8/emu"
 	"github.com/valep27/GChip8/io"
@@ -13,8 +14,22 @@ func main() {
 	chip8 := emu.New()
 	front := io.NewSdlFrontend()
 
+	input := io.NewSdlInput()
+
 	front.Initialize()
+
 	defer front.Close()
 
-	front.Draw(chip8)
+	front.Draw(&chip8)
+
+	running := true
+	for running {
+		k := input.Poll()
+
+		if k == io.KEY_QUIT {
+			running = false
+		}
+
+		time.Sleep(33 * time.Millisecond)
+	}
 }
