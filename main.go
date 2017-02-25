4725fb642dfa64d4aacd4d4fbdad3fa197eb397b
Valerio Pipolo <valep27@gmail.com>
2017-02-25 19:21:26 +0100

    Add loading from args and reorganize main loop

--- main.go
+++ main.go
@@ -6,30 +6,35 @@ import (
 
 	"github.com/valep27/GChip8/emu"
 	"github.com/valep27/GChip8/io"
+	"os"
 )
 
 func main() {
 	fmt.Println("Starting emulator...")
+	path := ""
+	if (len(os.Args) == 2) {
+		path = os.Args[1]
+	}
 
 	chip8 := emu.New()
+	chip8.LoadRom(path)
+
 	front := io.NewSdlFrontend()
-
 	input := io.NewSdlInput()
-
 	front.Initialize()
-
 	defer front.Close()
 
-	front.Draw(chip8)
 
 	running := true
 	for running {
-		k := input.Poll()
+		chip8.Step()
 
+		front.Draw(chip8)
+		
+		k := input.Poll()
 		if k == io.KEY_QUIT {
 			running = false
 		}
-
 		time.Sleep(33 * time.Millisecond)
 	}
 }
